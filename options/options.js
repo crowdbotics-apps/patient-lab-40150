81b78474cbca92d4c370847c782e2ba6ba1e2726
Crowdbotics <team@crowdbotics.com>
2023-04-23 23:25:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "patient-lab-40150",
+  url: "https://patient-lab-40150.botics.co",
+  api: "https://patient-lab-40150.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
